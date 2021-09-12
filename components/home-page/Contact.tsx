73cf6501824729446e67503a1b6849407fd8c789
Christoph Stenglein <christoph@cstenglein.com>
2021-09-12 08:04:39 +0200

    work on projects, make contact buttons responsive

--- components/home-page/Contact.tsx
+++ components/home-page/Contact.tsx
@@ -4,38 +4,38 @@ import { FC } from 'react';
 const Contact: FC = () => {
   return (
     <div className='bg-indigo-600'>
-      <section className='rounded-xl shadow-md bg-white text-black flex justify-center flex-col mx-5 xl:w-2/3 xl:mx-auto'>
+      <section className='flex flex-col justify-center mx-5 xl:w-2/3 xl:mx-auto rounded-xl shadow-md bg-white text-black'>
         <h4 className='my-10 text-2xl font-bold text-center'>Contact me</h4>
-        <ul className='list-none flex flex-col lg:flex-row items-center text-center justify-center pb-10 gap-3'>
-          <li>
-            <Link href='https://www.linkedin.com/in/christoph-tobias-stenglein-236453113/'>
-              <a className='contact-link' target='_blank' rel='noopener noreferrer'>
+        <article className='list-none flex flex-col lg:flex-row items-center text-center justify-center pb-10 px-4 gap-3'>
+          <Link href='https://www.linkedin.com/in/christoph-tobias-stenglein-236453113/' passHref>
+            <button className='contact-link'>
+              <a target='_blank' rel='noopener noreferrer'>
                 LinkedIn
               </a>
-            </Link>
-          </li>
-          <li>
-            <Link href='https://github.com/cstenglein'>
-              <a className='contact-link' target='_blank' rel='noopener noreferrer'>
+            </button>
+          </Link>
+          <Link href='https://github.com/cstenglein' passHref>
+            <button className='contact-link'>
+              <a target='_blank' rel='noopener noreferrer'>
                 GitHub
               </a>
-            </Link>
-          </li>
-          <li>
-            <Link href='https://www.xing.com/profile/ChristophTobias_Stenglein/cv'>
-              <a className='contact-link' target='_blank' rel='noopener noreferrer'>
+            </button>
+          </Link>
+          <Link href='https://www.xing.com/profile/ChristophTobias_Stenglein/cv' passHref>
+            <button className='contact-link'>
+              <a target='_blank' rel='noopener noreferrer'>
                 XING
               </a>
-            </Link>
-          </li>
-          <li>
-            <Link href='https://twitter.com/_cstenglein_'>
-              <a className='contact-link' target='_blank' rel='noopener noreferrer'>
+            </button>
+          </Link>
+          <Link href='https://twitter.com/_cstenglein_' passHref>
+            <button className='contact-link'>
+              <a target='_blank' rel='noopener noreferrer'>
                 Twitter
               </a>
-            </Link>
-          </li>
-        </ul>
+            </button>
+          </Link>
+        </article>
       </section>
     </div>
   );
--- components/home-page/Projects.tsx
+++ components/home-page/Projects.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
 
 const Projects: FC = () => {
-  return <section className='bg-indigo-600 py-5 text-white text-center'>
-    <h2 className='text-3xl'>Projects</h2>
-    <div>BLABLABLA</div>
-  </section>;
+  return (
+    <section className='bg-indigo-600 py-5 text-white text-center'>
+      <h2 className='text-3xl'>Projects</h2>
+      <article className='container flex flex-col lg:flex-row justify-center p-4 mx-auto gap-3'>
+        <article className='lg:w-1/3 bg-white text-black rounded p-5'>Raspiblitz-web</article>
+        <article className='lg:w-1/3 bg-white text-black rounded p-5'>This Website ;)</article>
+      </article>
+    </section>
+  );
 };
 
 export default Projects;
