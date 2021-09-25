9a98097b0b393f1c3a19d941302bfbf7e65ba0f1
Christoph Stenglein <christoph@cstenglein.com>
2021-09-25 19:21:57 +0200

    change imprint background and add title

--- pages/imprint.tsx
+++ pages/imprint.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import { FC, useState } from 'react';
 import { useContact } from '../hooks/useContact';
@@ -18,22 +19,34 @@ export const ImprintPage: FC = () => {
   };
 
   return (
-    <main className='flex justify-center mt-20 pt-5'>
-      <section className='px-10 pt-2 pb-5 bg-indigo-600 text-white rounded'>
-        <h4 className='text-xl font-bold my-2'>Imprint</h4>
-        <button className='border border-white rounded p-2 my-3 flex justify-around' onClick={showContactHandler}>
-          Toggle Contact Info&nbsp;
-          <Image src='/icons/user.svg' alt='User Icon' width={20} height={20} />
-        </button>
-        <article className='flex flex-col items-center justify-center'>
-          {showContact && contactInfo.split('\n').map((line, index) => <p key={index}>{line}</p>)}
-        </article>
-        <a href={link} onClick={clickHandler} className='border border-white rounded p-2 flex my-3 justify-around'>
-          Send me a mail&nbsp;
-          <Image src='/icons/envelope.svg' alt='Envelope Icon' width={20} height={20} />
-        </a>{' '}
-      </section>
-    </main>
+    <>
+      <Head>
+        <title>Christoph Stenglein - Imprint</title>
+      </Head>
+      <main className='flex justify-center items-start pt-20 bg-gradient-to-tr from-indigo-600 to-indigo-400 h-screen'>
+        <section className='px-10 py-5 bg-white text-black rounded mt-20'>
+          <h4 className='text-xl font-bold my-2 text-indigo-600'>Imprint</h4>
+          <button
+            className='border border-indigo-600 rounded p-2 my-3 flex justify-around'
+            onClick={showContactHandler}
+          >
+            Toggle Contact Info&nbsp;
+            <Image src='/icons/user.svg' alt='User Icon' width={20} height={20} />
+          </button>
+          <article className='flex flex-col items-center justify-center'>
+            {showContact && contactInfo.split('\n').map((line, index) => <p key={index}>{line}</p>)}
+          </article>
+          <a
+            href={link}
+            onClick={clickHandler}
+            className='border border-indigo-600 rounded p-2 flex my-3 justify-around'
+          >
+            Send me a mail&nbsp;
+            <Image src='/icons/envelope.svg' alt='Envelope Icon' width={20} height={20} />
+          </a>{' '}
+        </section>
+      </main>
+    </>
   );
 };
 
