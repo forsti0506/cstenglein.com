b7634cae0406519d6c7499cdf1cacad21158ddc2
Christoph Stenglein <christoph@cstenglein.com>
2021-09-25 19:06:14 +0200

    add title to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { FC } from 'react';
 import Contact from '../components/home-page/Contact';
 import Hero from '../components/home-page/Hero';
@@ -5,9 +6,12 @@ import Projects from '../components/home-page/Projects';
 import Skills from '../components/home-page/Skills';
 import Footer from '../components/navigation/Footer';
 
-export const HomePage: FC = (props) => {
+export const HomePage: FC = () => {
   return (
     <main className='mt-20 block'>
+      <Head>
+        <title>Christoph Stenglein - Full Stack Developer</title>
+      </Head>
       <Hero />
       <Skills />
       <Projects />
